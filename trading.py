02225a050ec9b74af03f285db9e9a6d4079e40ff
isaacglahou7-dotcom <isaacglahou7@gmail.com>
2026-08-07 15:26:07 +0000

    Create trading.py

---
+++ trading.py
@@ -0,0 +1,16 @@
+# =========================
+# FORMAT ANALYSE TRADING
+# =========================
+
+
+def format_analyse(resultat):
+
+    return f"""
+📊 ANALYSE TRADING AI PRO
+
+{resultat}
+
+
+⚠️ Ceci est une analyse automatique.
+Toujours vérifier avant de prendre une position.
+"""
