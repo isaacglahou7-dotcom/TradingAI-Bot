28d74d60425433bcc3c0a385b471f2d90197340a
isaacglahou7-dotcom <isaacglahou7@gmail.com>
2026-08-07 00:32:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+
 import asyncio
 import os
 
-TOKEN = os.getenv("BOT_TOKEN", "").strip()
+from aiohttp import web
+
+
+TOKEN = os.getenv("BOT_TOKEN", "").replace("\n", "").replace("\r", "").strip()
 
 if not TOKEN:
     raise ValueError("❌ BOT_TOKEN manquant dans les variables Render")
@@ -37,9 +41,42 @@ async def analyse_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+# =========================
+# SERVEUR POUR RENDER
+# =========================
+
+async def health_check(request):
+    return web.Response(text="Trading AI Bot OK")
+
+
+async def start_web_server():
+    app_web = web.Application()
+
+    app_web.router.add_get("/", health_check)
+
+    runner = web.AppRunner(app_web)
+    await runner.setup()
+
+    port = int(os.environ.get("PORT", 10000))
+
+    site = web.TCPSite(
+        runner,
+        "0.0.0.0",
+        port
+    )
+
+    await site.start()
+
+
+# =========================
+# TELEGRAM BOT
+# =========================
+
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+app.add_handler(
+    CommandHandler("start", start)
+)
 
 app.add_handler(
     MessageHandler(filters.PHOTO, analyse_image)
@@ -47,7 +84,10 @@ app.add_handler(
 
 
 async def main():
+
     print("🤖 Bot démarré...")
+
+    await start_web_server()
 
     await app.initialize()
     await app.start()
